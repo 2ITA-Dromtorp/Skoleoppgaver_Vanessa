991c50eac15b0efb12043aebbd3f695cb3e51a86
Vanessa <nybruker@Nessas-MacBook-Pro.local>
2023-09-01 15:08:09 +0200

    Intro React project

---
+++ GrunnleggendeProgrammering/grunnleggende_programmering/src/oppgave1.js
@@ -0,0 +1,85 @@
+
+function DelOppgaveA () {
+
+
+    return (
+        <div className="delOppgave">
+        <h2>Deloppgave A</h2>
+        <p >Hva skriver du i javascript dersom du vil gi variabelen test verdien 8? Hvilken datatype er dette?</p>
+        <p id="oppg1">Svar: let test = 8</p>
+        <p>Svar: Se i console loggen nå {console.log(typeof(8))}</p>
+        <p>  </p>
+        </div>
+    );
+}
+
+function DeloppgaveB () {
+
+    return (
+        <div>
+            <h2>Deloppgave B</h2>
+            <p>Hva skriver du dersom du vil gi variabelen test verdien "testverdi"? Hvilken datatype er dette?</p>
+            <p>Svar: let test = testverdi</p>
+            <p>Svar: Datatypen er da en string også kjent som tekst-string</p>
+        </div>
+    )
+}
+
+function DeloppgaveC () {
+
+    return (
+        <div>
+            <h2>Deloppgave C</h2>
+            <p>Hva skriver du dersom du vil regne ut 2 * 3 og sette resultatet inn i variabelen produkt?</p>
+            <p>Svar: let produkt = 2*3 </p>
+            <p>Svar: Se console loggen  </p>
+        </div>
+    )
+}
+
+let produkt = 2*3
+console.log('2*3 er '+ produkt)
+
+
+function DeloppgaveD () {
+
+    return (
+        <div>
+            <h2>Deloppgave D</h2>
+            <p>Hva skriver du dersom du vil regne ut verdien av brøken 2/3 og sette resultatet inn i variabelen broek? </p>
+            <p>Svar: let broek = 2/3 </p>
+            <p>Svar: Se console loggen  </p>
+        </div>
+    )
+}
+
+let broek = 2/3
+console.log('Verdien av 2/3 er ' + broek);
+
+
+function DeloppgaveE () {
+
+   let produkt
+}
+
+
+
+
+
+
+
+function Oppgave1 () {
+
+    return (
+        <>
+        <h1>Dette er oppgave 1</h1>
+        <DelOppgaveA />
+        <DeloppgaveB />
+        <DeloppgaveC />
+        <DeloppgaveD />
+        </>
+    )
+
+}
+
+export default Oppgave1;
--- react_intro/react_intro/src/App.js
+++ react_intro/react_intro/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import logo from './images/stjerne.jpeg';
 import './App.css';
+import MyApp from './NewComponent';
+import { AboutPage } from './NewComponent';
+import Profile from './profile';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Dette er et komponent som heter App som ligger i App.js</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MyApp />
+        <AboutPage />
+        <Profile />
       </header>
     </div>
   );
 }
 
 export default App;
+
