f0b4f02236df4e22e5c3eb12d5627e00dccb1882
Vanessa <nybruker@Nessas-MacBook-Pro.local>
2023-09-19 14:00:29 +0200

    SSP game oppgaver 19.09

--- GrunnleggendeProgrammering/grunnleggende_programmering/src/Stein.js
+++ GrunnleggendeProgrammering/grunnleggende_programmering/src/Stein.js
@@ -1,9 +1,29 @@
+import maskin_papir from './images/maskin_papir.png';
+import maskin_saks from './images/maskin_saks.png';
+import maskin_stein from './images/maskin_stein.png';
+import maskin_ukjent from './images/maskin_ukjent.png';
+import spiller_papir from './images/spiller_papir.png';
+import spiller_saks from './images/spiller_saks.png';
+import spiller_stein from './images/spiller_stein.png';
+import { useState, useEffect } from "react";
+
+
+
 
 export default function Stein () {
 
     return (
-        <div>
+        <div className='sspGame'>
             <h2>Stein - Saks - Papir</h2>
+            <img src={maskin_ukjent}></img>
+            <button>Play</button>
+            <img src={maskin_ukjent}></img>
+            <div className='sspButton'>
+                <button>Stein</button>
+                <button>Saks</button>
+                <button>Papir</button>
+            </div>
+
         </div>
     )
 }
