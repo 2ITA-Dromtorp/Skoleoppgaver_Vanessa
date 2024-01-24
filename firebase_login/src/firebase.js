3dc8ffbaf4c5634976cf6c0dc383223706f0c0a9
Vanessa <nybruker@Nessas-MacBook-Pro.local>
2024-01-24 13:58:06 +0100

    mlem

---
+++ firebase_login/src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCnCeJYA_KGBk3JF-cM5G7i0_pokygjBG4",
+  authDomain: "reactapp-e10b6.firebaseapp.com",
+  databaseURL: "https://reactapp-e10b6-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "reactapp-e10b6",
+  storageBucket: "reactapp-e10b6.appspot.com",
+  messagingSenderId: "927539741560",
+  appId: "1:927539741560:web:9d42f265f2ebb6448cbe8d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
