caed9c0a39445e102b0f288e14016833348fd631
Vanessa <nybruker@Nessas-MacBook-Pro.local>
2024-01-24 13:32:04 +0100

    Ferdig design på firebase login oppgave, hei Matheo hvis du ser denne meldingen

--- firebase_login/src/App.js
+++ firebase_login/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function () {
+
+    /*const navigate = useNavigate();
+
+    const [username, setUsername] = useState("");
+    const [passord, setPassord] = useState("");
+
+    const handleSubmit = () => {
+        console.log(username, passord);
+
+
+    const dataToSend = {
+        username: username,
+        passord: passord
+    }
+        fetch('/login', {
+            method:'POST',
+            headers:{
+              "content-type":"application/json",
+            },
+            body:JSON.stringify(dataToSend),
+        })
+        .then(async (res) => {
+            if(res.status===401) {
+                alert("Feil brukernavn eller passord")
+            }
+            if(res.status===200) {
+                        const data = await res.json();                    
+                        console.log(data)
+                        navigate("/hjem")
+                }})
+                    .catch((error) => {
+                        console.error('Error fetching data:', error);
+                    });
+        
+    }
+*/
+
+
+    return (
+        <div className='content'> 
+          <div className='child'>
+            <h2 className='h2'>Logg inn</h2>
+            <div className='signup-input'>
+                <p>Skriv inn brukernavn</p>
+                <input type='text' />  
+                <p>Skriv inn passord</p>
+                <input type='password'/>  
+                <button className='login_button'>Logg inn</button>
+             </div>
+
+             <h5 className='under-content'>Har du ikke en brukerkonto? <a href="/signup">Opprett brukerkonto</a></h5>
+            </div>
+          </div>
+    )
+       
 }
-
-export default App;
